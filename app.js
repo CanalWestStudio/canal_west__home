b1476ca252b5de6a17dabefdc291b0f245e1dcc6
mcrundo <matt3r@gmail.com>
2018-04-09 02:39:10 -0400

    update copy, partials, intro assets

--- app.js
+++ app.js
@@ -1,26 +1,24 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var stylus = require('stylus');
-
-var index = require('./routes/index');
-var users = require('./routes/users');
-
-var app = express();
+var express = require('express'),
+		cors = require('cors'),
+		path = require('path'),
+		favicon = require('serve-favicon'),
+		logger = require('morgan'),
+		cookieParser = require('cookie-parser'),
+		bodyParser = require('body-parser'),
+		stylus = require('stylus'),
+		index = require('./routes/index'),
+		app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(favicon(path.join(__dirname + '/public/images/favicon.png')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use('/static', express.static(__dirname + '/public'));
 app.use(stylus.middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+  	title: 'Canal West Studios',
+  	subtitle: 'Comprehensive Consulting Services',
+  	location: 'Washington, DC',
+  	address: '1101 Connecticut Ave.',
+  	suite: 'Suite 450',
+  	zip: '20036'
+  });
 });
 
 module.exports = router;
